60ec8b0e743d70c6d26f725b8e4e5844df8b02da
Kyuho Lee <modulo641@gmail.com>
2020-05-17 03:30:57 +0900

    Version 0.1.1
    Add Force Name Search Option for the compatibility with Theatre Insert module

--- chat-portrait.js
+++ chat-portrait.js
@@ -1,16 +1,47 @@
 class PortraitsOnChatMessage {
   static onRenderChatMessage(chatMessage, html, messageData) {
-    let actor = game.actors.get((messageData.message.speaker.actor));
+    let speaker = messageData.message.speaker
+    var actor;
+    if (speaker.token) {
+      actor = game.actors.tokens[speaker.token];
+    }
+    
+    if (!actor) {
+      actor = game.actors.get((speaker.actor));
+    }
+
+    const forceNameSearch = game.settings.get('ChatPortrait', 'forceNameSearch');
+    if (!actor && forceNameSearch) {
+      game.actors.forEach((value) => {
+        if (value.name === speaker.alias) {
+          actor = value;
+        }
+      });
+    }
+
     if (actor) {
       let img = document.createElement("img");
       img.src = actor.img;
       img.width = 36;
       img.height = 36;
-      img.style.border = `2px solid ${messageData.author.data.color}`;
+      let authorColor = messageData.author ? messageData.author.data.color : "black";
+      img.style.border = `2px solid ${authorColor}`;
       let element = html.find(".message-header")[0];
       element.prepend(img);
     }
   }
 }
+
+Hooks.once('init', () => {
+  game.settings.register('ChatPortrait', 'forceNameSearch', {
+    name: game.i18n.localize("chat-portrait.force-name-search-s"),
+    hint: game.i18n.localize("chat-portrait.force-name-search-l"),
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean,
+    onChange: forceNameSearch => window.location.reload()
+  });
+});
 
 Hooks.on('renderChatMessage', PortraitsOnChatMessage.onRenderChatMessage);
